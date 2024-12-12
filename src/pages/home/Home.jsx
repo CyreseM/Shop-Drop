3a1ddb937584193fab7a7d717056a92088eb17f8
Cyrese Mills <cmills@persol.com>
2024-12-12 10:06:59 +0000

    boogie woogie

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import Categories from "../../components/categories/Categories";
 import Slider from "../../components/slider/Slider";
 import NewArrivals from "../../components/products/NewArrivals";
 import Bestsellers from "../../components/products/Bestsellers";
+import NewsLetter from "../../components/newsletter/NewsLetter";
+import Footer from "../../components/footer/Footer";
 function Home() {
   return (
     <div>
@@ -13,6 +15,8 @@ function Home() {
       <Categories />
       <NewArrivals />
       <Bestsellers />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 }
